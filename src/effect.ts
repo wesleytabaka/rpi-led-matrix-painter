deac869c7847b07b129fc3f04a3f0dcfa8510fa3
Wesley Tabaka <wt@wesleytabaka.com>
2021-05-11 23:49:00 -0400

    Change Effect to interface

--- src/effect.ts
+++ src/effect.ts
@@ -1,13 +1,7 @@
 import { EffectOptions } from "./effectoptions";
 import { EffectType } from "./effecttype";
 
-export class Effect {
+export interface Effect {
     effectType: EffectType
     effectOptions: EffectOptions;
-
-    constructor(effectType: EffectType, effectOptions: EffectOptions){
-        this.effectType = effectType;
-        this.effectOptions = effectOptions;
-    }
-
 }
